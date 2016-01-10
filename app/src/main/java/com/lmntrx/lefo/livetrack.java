c0d7537c6926a542f54c70ad095e90493201ce08
livin21 <livinmathew99@gmail.com>
2016-01-10 22:15:37 +0530

    Blueprint Added

---
+++ app/src/main/java/com/lmntrx/lefo/livetrack.java
@@ -0,0 +1,13 @@
+package com.lmntrx.lefo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class livetrack extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_livetrack);
+    }
+}
